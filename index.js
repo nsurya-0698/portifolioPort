db5fca5ab866ee5a6da8b5368478cc3689eda1bb
nsurya-0698 <nsuryateja1997@gmail.com>
2024-07-05 20:46:44 -0500

    files arrangment

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './personal-portifolio/src/App';
+import reportWebVitals from './personal-portifolio/src/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
