48cdcb8e67f284a9d4a6d648aaea1509ce65ee86
nsurya-0698 <nsuryateja1997@gmail.com>
2024-07-09 18:18:31 -0500

    Updated Main session with new content and updated Get in toush button

--- personal-portifolio/src/components/Main/index.js
+++ personal-portifolio/src/components/Main/index.js
@@ -8,13 +8,12 @@ const Main = () => {
                 <h1 className="name-header">Surya Teja Nammi</h1>
                 <h1 className="passion-header">Building Robust Full Stack Solutions</h1>
                 <p className="statement">I'm a Software Engineer with 5+ years of expertise in full stack development. I specialize in creating and optimizing platform performance, leading global upgrade projects, and developing robust, scalable applications. Currently, I focus on building accessible, human-centered products.</p>
-                <a href="mailto:tejanamminm1@gmail.com">
-                    <button className="get-in-touch">Hire Me</button>
+                <a href="#contact" className="nav-things">
+                    <button className="get-in-touch">Get in touch</button>
                 </a>
             </div>
         </div>
     )
 }
-
 
 export default Main;
